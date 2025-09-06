828090c38841a5d816092cbf8d02afcf57694ea5
Dumitru-Bobeico <dima.bobeico.2003@gmail.com>
2025-09-06 11:38:12 +0300

    made health bar

--- FirstPersonShooter/Assets/_FirstPersonShooter/Scripts/Health.cs
+++ FirstPersonShooter/Assets/_FirstPersonShooter/Scripts/Health.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public int health;
     
+    public RectTransform healthBar;
+    private float originalHealthBarSize;
+    
     [Header("UI")]
     public TextMeshProUGUI healthText;
+
+    private void Start()
+    {
+        originalHealthBarSize = healthBar.sizeDelta.x;
+    }
 
     public void TakeDamage(int damage)
     {
         health -= damage;
+        
+        healthBar.sizeDelta = new Vector2(originalHealthBarSize * health / 100f, healthBar.sizeDelta.y);
 
         healthText.text = health.ToString();
         
